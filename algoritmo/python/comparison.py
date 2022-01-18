f6407c276b97198a3464f03e4b91026a35b47aa5
iasmimaraujol <iasmim.engenharia@gmail.com>
2022-01-18 19:03:05 -0300

    1 day

--- algoritmo/python/comparison.py
+++ algoritmo/python/comparison.py
@@ -1,4 +1,4 @@
-def countSort(arr):
+"""def countSort(arr):
  
     # The output character array that will have sorted arr
     output = [0 for i in range(len(arr))]
@@ -29,7 +29,31 @@ def countSort(arr):
     # contains sorted characters
     for i in range(len(arr)):
         ans[i] = output[i]
-    return ans
+    return ans"""
+def countSort(array_):
+    #take the max. elemnt
+    max_ = max(array_)
+    #inicializate an array with max + 1
+    cont = []
+    #output array
+    out = []
+    for i in range(max_ + 1):
+        cont.append(0)
+        out.append(0)
 
+    #print(max_, cont)
+    #store the count of each element
+    for i in range(max_ + 1):
+        cont[i] = i + cont[i - 1]
+    #print(cont)
+    for i in array_:
+        aux = int(i)        
+        aux2 = cont[aux]     
+        aux3 = aux2 - 1
+        print(aux, aux2, aux3)
+        out[aux3] = i
+    #print(out)
+    print(cont)
+    return array_
 l1 = [60,35,81,98,14,87]
 print(countSort(l1))
